d10a4c6167d2927d79759f9886dec872e632d7b4
Urik Paul Rodríguez Pérez <54944235+ie715536@users.noreply.github.com>
2019-09-17 23:12:32 -0500

    Update tarea4_1.asm

--- tarea4_1.asm
+++ tarea4_1.asm
@@ -1,49 +1,45 @@
 .data 
 vector1: .word 1 2 3
-vector2: .word 4 5 6
-result:
+vector2: .word 6 7 8
+
 .text
 main:
-     addi $t3, $zero, 8
-     addi $t2, $t3, 5
-
-     la $a1, vector1
-     la $a2, vector2
-     la $a0, result
-     jal sum
-     
-     j exit
-
-sum:
-    addi $sp, $sp, -20
-    sw $t0, 0($sp)
-    sw $t1, 4($sp)
-    sw $t2, 8($sp)
-    sw $t3, 12($sp)
-    sw $t4, 16($sp)
+    addi $s0, $zero, 0  #mulRes
     
-    addi $t0, $zero, 0
+    addi $t0, $zero, 0  #i
+    addi $t1, $zero, 0  #acc
+    addi $t2, $zero, 3  #k
+    addi $t4, $zero, 0  #registro temporal vector1
+    addi $t5, $zero, 0  #registro temporal vector2 
+ 
+    la $a1, vector1  #guarda la primer dirección del vector1 en a1
+    la $a2, vector2  #guarda la primer dirección del vector2 en a2
+    
+    jal for 
+    
 for:
-    lw $t2,0($a1)
-    lw $t3,0($a2)
-    add $t4,$t2,$t3
-    sw $t4,0($a0)
     
-    addi $a1,$a1,4
-    addi $a0,$a0,4
-    addi $a2,$a2,4
+    slt $t3, $t0, $t2  # $t4 comprueba si es i menor que k    
+    beq $t3, $zero, exit  
+    sll $t4, $t0, 2  #desplazas i y lo guardas en el registro temporal
+    sll $t5, $t0, 2 
+    addi $t0, $t0, 1  # avanzas i
     
-    addi $t0, $t0, 1	
-    slti $t1, $t0, 3
-    bne $t1, $zero,  for
+    add $t4, $t4, $a1 #Posicionas el registro temporal en la posición deseada del vector1
+    add $t5, $t5, $a2 #Posicionas el registro temporal en la posición deseada del vector2
+    lw $t6, 0($t4)    #Obtienes el valor de la posición seleccionada
+    lw $t7, 0($t5)    #Obtienes el valor de la posición seleccionada
     
-    lw $t0, 0($sp)
-    lw $t1, 4($sp)
-    lw $t2, 8($sp)
-    lw $t3, 12($sp)
-    lw $t4, 16($sp)
-    addi $sp, $sp, +20
-                      
+    
+    
+    
+multAndAcumulate: #multiplica $t5 * $t6 las posiciones del vector
+        
+    mul $s0, $t6, $t7
+    
+Acumulate: # sumas acc con la multiplicación actual
+    
+    add $t1, $t1, $s0
     jr $ra
-    
-exit:
+        
+exit: #El valor del producto punto se guarda en $t1
