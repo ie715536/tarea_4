30102f8ecdab50f9c7e60233db9e2966c5335487
Urik Paul Rodríguez Pérez <54944235+ie715536@users.noreply.github.com>
2019-09-20 20:45:08 -0500

    Update tarea4_4.asm

--- tarea4_4.asm
+++ tarea4_4.asm
@@ -6,12 +6,12 @@ main:
     addi $t1, $zero, 4  #n
     jal Pow
 Pow:
-    beq $t1, $zero, index0
+    beq $t1, $zero, index0  #Se compara si el exponente es 0 desde un inicio
     
-    seq $t3, $t1, 1
+    seq $t3, $t1, 1  #Cuando el exponente sea igual a 1 se acaba el programa 
     bne $t3, $zero, exit 
 
-    mul $t0, $t0, 2
+    mul $t0, $t0, 2  #Función recursiva Pow, multiplica por dos y se repite reduciendo la n
     subi $t1, $t1, 1
     jr $ra
     
